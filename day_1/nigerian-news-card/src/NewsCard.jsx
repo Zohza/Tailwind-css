417b926fb31d0b3077a281435a41f917a5829657
Boluwatife <gbadamosiboluwatife2002@gmail.com>
2025-11-13 11:15:31 +0100

    updated

--- day_1/nigerian-news-card/src/NewsCard.jsx
+++ day_1/nigerian-news-card/src/NewsCard.jsx
@@ -13,13 +13,20 @@ Create a news article card with:
 - Author name and date
 - Read time indicator
 - Read more link */}
-<nav className="flex h-20 bg-purple-600 w-600">
-<ul className='flex gap-15 '>
-    <li>Politics</li>
-    <li>Sports</li>
-    <li>Entertainment</li>
-</ul>
-</nav>
+<p className='font-bold text-center'>Politics</p>
+<img src="src/assets/news.jpeg" alt="Benue commisioner News" className="flex h-50 bg-purple-600 shadow-lg flex-column align-center justify-center h-5  mx-auto bg-gray-50 w-89 rounded-tl-lg
+rounded-tr-lg " />
+<p className='shadow-lg flex-column align-center justify-center h-110  mx-auto bg-gray-50 rounded-2xl w-89'>
+  <p className='font-bold text-center text-lg'>Benue Commisoner News</p>
+  Benue Commissioner for Cooperatives and Rural Development, Dr Yanmar Ortese, says the establishment of the Cooperative Development Fund by the Cooperative Financing Agency of Nigeria (CFAN) will provide easy access to scalable and affordable loans, boosting grassroots development.
+
+Ortese stated this on Wednesday in Makurdi during the ninth CFAN summit, themed: “Cooperative Development Fund: A one-stop shop for cooperative financing in Nigeria.”
+
+Represented by the Ministry’s Permanent Secretary, Mr Jonathan Asen, the commissioner said cooperatives have long been the unsung heroes of grassroots development.
+
+<div className="flex align-center justify-center p-2 bg-purple-600 w-20 mx-auto rounded-2xl mt-6">Next..</div>
+</p>
+
     </div>
   )
 }
